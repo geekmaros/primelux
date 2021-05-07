d8deaf7a3dbb2d5712aa2cb6096064124f06b459
mustaphic <abdulrasaq.mustapha@demzanalytics.com>
2021-05-07 10:29:24 +0100

    added seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    titleTemplate: 'Mastering Nuxt: %s',
     title: 'primelux',
     meta: [
       { charset: 'utf-8' },
